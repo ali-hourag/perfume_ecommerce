6748065930763667fd3c2b25c75ae054d6d019c7
Ali Hourag <ali.hourag@gmail.com>
2023-07-05 20:25:03 +0200

    feat(register): do register validation

--- src/pages/profile/register/Register.tsx
+++ src/pages/profile/register/Register.tsx
@@ -1,34 +1,98 @@
+import { useForm } from "react-hook-form";
 import "./register.css";
 
 export const Register = () => {
+    const { register, handleSubmit, formState: { errors }, reset, watch } = useForm({
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            passwordConfirmation: ""
+        }
+    })
+
+    const onSubmit = () => {
+        console.log("entro");
+        reset();
+    }
+
     return (
         <>
-            <form className="form-container form-container-register">
+            <form onSubmit={handleSubmit(onSubmit)} className="form-container form-container-register">
                 <div className="form-entry-container">
                     <label className="form-entry-label">Name</label>
-                    <input type="text" id="name-input" name="name-input"
+                    <input type="text" id="name-input"
                         className="form-input"
                         placeholder="enter name"
+                        {...register("name", {
+                            required: {
+                                value: true,
+                                message: "name required"
+                            },
+                            pattern: {
+                                value: /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
+                                message: "invalid name"
+                            }
+                        })}
                     />
-                    <p></p>
+                    {errors.name && <p>{errors.name.message}</p>}
                 </div>
                 <div className="form-entry-container">
                     <label className="form-entry-label">Email</label>
-                    <input type="email" id="email-input" name="email-input"
+                    <input type="email" id="email-input"
                         className="form-input"
                         placeholder="enter email"
+                        {...register("email", {
+                            required: {
+                                value: true,
+                                message: "email required"
+                            },
+                            pattern: {
+                                value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                message: "invalid email"
+                            }
+                        })}
                     />
-                    <p></p>
+                    {errors.email && <p className="error-p">{errors.email.message}</p>}
                 </div>
                 <div className="form-entry-container">
                     <label className="form-entry-label">password</label>
-                    <input type="password" id="password-input" name="password-input"
+                    <input type="password" id="password-input"
                         className="form-input"
                         placeholder="enter password"
+                        {...register("password", {
+                            required: {
+                                value: true,
+                                message: "password required"
+                            },
+                            pattern: {
+                                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+                                message: "invalid password"
+                            }
+                        })}
                     />
-                    <p></p>
+                    {errors.password && <p className="error-p">{errors.password.message}</p>}
                 </div>
-                <button className="login-btn">Register</button>
+                <div className="form-entry-container">
+                    <label className="form-entry-label">confirm password</label>
+                    <input type="password" id="password-confirmation-input"
+                        className="form-input"
+                        placeholder="enter password"
+                        {...register("passwordConfirmation", {
+                            required: {
+                                value: true,
+                                message: "password required"
+                            },
+                            pattern: {
+                                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+                                message: "invalid password"
+                            },
+                            validate: value => value === watch("password") || "passwords do not match"
+                        })}
+                    />
+                    {errors.passwordConfirmation && <p className="error-p">{errors.passwordConfirmation.message}</p>}
+                </div>
+                <button type="submit" className="login-btn">Register</button>
             </form>
         </>
     )
