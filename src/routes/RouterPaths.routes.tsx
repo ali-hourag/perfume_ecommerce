603cb72a3d0344e240f14fad68f5bee3546157b7
Ali Hourag <ali.hourag@gmail.com>
2023-07-06 05:56:32 +0200

    feat(cart && wish routes): create cart and wishlist routes and components

--- src/routes/RouterPaths.routes.tsx
+++ src/routes/RouterPaths.routes.tsx
@@ -5,8 +5,10 @@ import { HomePage } from "../pages/homePage/HomePage";
 import { ProductsPage } from "../pages/productsPage/ProductsPage";
 import { ProductSelected } from "../pages/productSelected/ProductSelected";
 import { Profile } from "../pages/profile/Profile";
-import { Login } from "../pages/profile/login/login";
+import { Login } from "../pages/profile/login/Login";
 import { Register } from "../pages/profile/register/Register";
+import { Cart } from "../pages/cart/Cart";
+import { Wishlist } from "../pages/wishlist/Wishlist";
 
 export const RouterPaths: FC = () => {
 
@@ -22,8 +24,8 @@ export const RouterPaths: FC = () => {
                             <Route index element={<Login />} />
                             <Route path="register" element={<Register />} />
                         </Route>
-                        <Route path="cart" />
-                        <Route path="wishlist" />
+                        <Route path="cart" element={<Cart />} />
+                        <Route path="wishlist" element={<Wishlist />} />
                         <Route path="*" element={<Navigate replace to="/" />} />
                     </Route>
                 </Routes>
